1907d0d25d7564c0059c7590d9174dafaea17ebb
Roberto Perez Alcolea <rperezalcolea@netflix.com>
2019-06-18 16:37:18 -0700

    Always create a new group matcher and add exception details (#103)
    * Always create a new group matcher and add exception details
    
    * remove matcher reset since we always create a new group matcher

--- src/main/kotlin/nebula/plugin/resolutionrules/extensions.kt
+++ src/main/kotlin/nebula/plugin/resolutionrules/extensions.kt
@@ -1,10 +1,5 @@
 package nebula.plugin.resolutionrules
 
-import java.util.regex.Matcher
-
 inline fun <T, R> Collection<T>.mapToSet(transform: (T) -> R): Set<R> {
     return mapTo(LinkedHashSet<R>(size), transform)
 }
-
-fun Matcher.matches(input: CharSequence) =
-        reset(input).matches()
