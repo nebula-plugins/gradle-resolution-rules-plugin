8f1852adc6ce361b5562500af8559b10231ef659
Roberto Perez Alcolea <rperezalcolea@netflix.com>
2019-06-18 15:12:54 -0700

    Always create a new group matcher and add exception details

--- src/main/kotlin/nebula/plugin/resolutionrules/alignRule.kt
+++ src/main/kotlin/nebula/plugin/resolutionrules/alignRule.kt
@@ -62,11 +62,10 @@ data class AlignRule(val name: String?,
     fun ruleMatches(dep: ModuleVersionIdentifier) = ruleMatches(dep.group, dep.name)
 
     fun ruleMatches(inputGroup: String, inputName: String): Boolean {
-        if (groupMatcher == null) {
-            groupMatcher = safelyCreatesMatcher(group, inputGroup, "group")
-            includesMatchers = includes.map { safelyCreatesMatcher(it, inputName, "includes") }
-            excludesMatchers = excludes.map { safelyCreatesMatcher(it, inputName, "excludes") }
-        }
+        groupMatcher = safelyCreatesMatcher(group, inputGroup, "group")
+        includesMatchers = includes.map { safelyCreatesMatcher(it, inputName, "includes") }
+        excludesMatchers = excludes.map { safelyCreatesMatcher(it, inputName, "excludes") }
+
 
         return safelyMatches(groupMatcher!!, inputGroup, "group") &&
                 (includes.isEmpty() || includesMatchers.any { safelyMatches(it, inputName, "includes") }) &&
@@ -78,7 +77,7 @@ data class AlignRule(val name: String?,
             it.toPattern().matcher(input)
         } catch (e: Exception) {
             throw java.lang.IllegalArgumentException("Failed to use regex '$it' from type '$type' to create matcher for '$input'\n" +
-                    "Rule: ${this}")
+                    "Rule: ${this}", e)
         }
     }
 
@@ -87,7 +86,7 @@ data class AlignRule(val name: String?,
             it.matches(input)
         } catch (e: Exception) {
             throw java.lang.IllegalArgumentException("Failed to use matcher '$it' from type '$type' to match '$input'\n" +
-                    "Rule: ${this}")
+                    "Rule: ${this}", e)
         }
     }
 }
