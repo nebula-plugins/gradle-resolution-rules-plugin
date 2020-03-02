bf3a1ebaa0ada24eb1872dfcfdf403736a27b043
Aubrey Chipman <achipman@netflix.com>
2020-03-02 10:14:18 -0800

    Alignment verification when migrating a dependency from one coordinate to another

--- src/integTest/groovy/nebula/plugin/resolutionrules/AbstractAlignRulesSpec.groovy
+++ src/integTest/groovy/nebula/plugin/resolutionrules/AbstractAlignRulesSpec.groovy
@@ -17,9 +17,73 @@ abstract class AbstractAlignRulesSpec extends IntegrationTestKitSpec {
                 resolutionRules files('$rulesJsonFile')
             }
         """.stripIndent()
+    }
 
-        settingsFile << """\
-            rootProject.name = '${moduleName}'
-        """.stripIndent()
+    protected def createAlignAndReplaceRules(Map<String, String> modulesAndReplacements) {
+        String reason = "★ custom replacement reason"
+        rulesJsonFile << """
+{
+    "replace": [
+""".stripIndent()
+
+        List<String> replacements = new ArrayList<>()
+        modulesAndReplacements.each { module, with ->
+            replacements.add("""        {
+            "module" : "$module",
+            "with" : "$with",
+            "reason" : "$reason",
+            "author" : "Test user <test@example.com>",
+            "date" : "2020-02-27T10:31:14.321Z"
+        }""")
+
+        }
+        rulesJsonFile << replacements.join(',')
+
+        rulesJsonFile << """
+    ],
+    "align": [
+        {
+            "group": "(test.nebula|test.nebula.ext)",
+            "reason": "Align test.nebula dependencies",
+            "author": "Example Person <person@example.org>",
+            "date": "2020-02-27T10:31:14.321Z"
+        }
+    ]
+}
+""".stripIndent()
+    }
+
+    protected def createAlignAndSubstituteRules(Map<String, String> modulesAndSubstitutions) {
+        String reason = "★ custom substitution reason"
+        rulesJsonFile << """
+{
+    "substitute": [
+""".stripIndent()
+
+        List<String> substitutions = new ArrayList<>()
+        modulesAndSubstitutions.each { module, with ->
+            substitutions.add("""        {
+            "module" : "$module",
+            "with" : "$with",
+            "reason" : "$reason",
+            "author" : "Test user <test@example.com>",
+            "date" : "2020-02-27T10:31:14.321Z"
+        }""")
+        }
+
+        rulesJsonFile << substitutions.join(',')
+
+        rulesJsonFile << """
+    ],
+    "align": [
+        {
+            "group": "(test.nebula|test.nebula.ext)",
+            "reason": "Align test.nebula dependencies",
+            "author": "Example Person <person@example.org>",
+            "date": "2020-02-27T10:31:14.321Z"
+        }
+    ]
+}
+""".stripIndent()
     }
 }
---
+++ src/integTest/groovy/nebula/plugin/resolutionrules/AlignAndMigrateViaSubstitutionSpec.groovy
@@ -0,0 +1,40 @@
+package nebula.plugin.resolutionrules
+
+
+import spock.lang.Unroll
+
+class AlignAndMigrateViaSubstitutionSpec extends AbstractAlignAndMigrateSpec {
+    @Unroll
+    def 'substitution and alignment | core alignment: #coreAlignment'() {
+        given:
+        createAlignAndSubstituteRules(['other:e:4.0.0': 'test.nebula:c:1.0.1'])
+
+        when:
+        def results = runTasks(*dependencyInsightTasks(coreAlignment))
+
+        then:
+        results.output.contains("test.nebula:a:1.0.0 -> $alignedVersion")
+        results.output.contains("test.nebula:b:$alignedVersion")
+        results.output.contains("other:e:4.0.0 -> test.nebula:c:$alignedVersion")
+        results.output.contains("substitution from 'other:e:4.0.0' to 'test.nebula:c:1.0.1' because ★ custom substitution reason")
+
+        if(coreAlignment) {
+            results.output.contains("belongs to platform aligned-platform")
+        } else {
+            results.output.contains("by rules aligning group")
+        }
+
+        when:
+        def dependenciesTasks = ['dependencies', '--configuration', 'compileClasspath']
+        if(coreAlignment) {
+            dependenciesTasks.add('-Dnebula.features.coreAlignmentSupport=true')
+        }
+        def resultsForDependencies = runTasks(*dependenciesTasks)
+
+        then:
+        resultsForDependencies.output.contains("other:e:4.0.0 -> test.nebula:c:$alignedVersion")
+
+        where:
+        coreAlignment << [false, true]
+    }
+}
