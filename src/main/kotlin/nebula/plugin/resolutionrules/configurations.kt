af39f010fa8230442c8ad54d6951a5a5f12ba34a
Danny Thomas <dannyt@netflix.com>
2020-09-16 07:38:50 +1000

    Use getDeclaredFields0 to allow modifiers field access on Java 13. Fixes #126
    See:
    
    - https://github.com/powermock/powermock/issues/939
    - https://github.com/Vampire/powermock/blob/66ce9f77215bae68b45f35481abc8b52a5d5b6ae/powermock-reflect/src/main/java/org/powermock/reflect/internal/WhiteboxImpl.java

--- src/main/kotlin/nebula/plugin/resolutionrules/configurations.kt
+++ src/main/kotlin/nebula/plugin/resolutionrules/configurations.kt
@@ -6,7 +6,10 @@ import org.gradle.api.artifacts.Configuration
 import org.gradle.api.internal.artifacts.configurations.ConfigurationInternal
 import org.gradle.util.Path
 import java.lang.reflect.Field
+import java.lang.reflect.InvocationTargetException
+import java.lang.reflect.Method
 import java.lang.reflect.Modifier
+
 
 /**
  * Various reflection hackiness follows due to deficiencies in the Gradle configuration APIs:
@@ -63,7 +66,27 @@ fun Any.setField(name: String, value: Any) {
     val field = javaClass.findDeclaredField(name)
     field.isAccessible = true
 
-    val modifiersField = Field::class.java.getDeclaredField("modifiers")
+    lateinit var modifiersField: Field
+    try {
+        modifiersField = Field::class.java.getDeclaredField("modifiers")
+    } catch (e: NoSuchFieldException) {
+        try {
+            val getDeclaredFields0 = Class::class.java.getDeclaredMethod("getDeclaredFields0", Boolean::class.javaPrimitiveType)
+            val accessibleBeforeSet: Boolean = getDeclaredFields0.isAccessible
+            getDeclaredFields0.isAccessible = true
+            @Suppress("UNCHECKED_CAST") val declaredFields = getDeclaredFields0.invoke(Field::class.java, false) as Array<Field>
+            getDeclaredFields0.isAccessible = accessibleBeforeSet
+            for (declaredField in declaredFields) {
+                if ("modifiers" == declaredField.name) {
+                    modifiersField = declaredField
+                    break
+                }
+            }
+        } catch (ex: Exception) {
+            e.addSuppressed(ex)
+            throw e
+        }
+    }
     modifiersField.isAccessible = true
     modifiersField.setInt(field, field.modifiers and Modifier.FINAL.inv())
 
