3b2eda812a7961f0410ba9ab665ba587be01aa56
Roberto Perez Alcolea <rperezalcolea@netflix.com>
2025-05-13 14:33:19 -0700

    NebulaResolutionRulesExtension: allow to configure include,exclude and optional through project properties

--- src/main/kotlin/nebula/plugin/resolutionrules/extensions.kt
+++ src/main/kotlin/nebula/plugin/resolutionrules/extensions.kt
@@ -1,5 +1,12 @@
 package nebula.plugin.resolutionrules
 
+import org.gradle.api.Project
+
 inline fun <T, R> Collection<T>.mapToSet(transform: (T) -> R): Set<R> {
     return mapTo(LinkedHashSet<R>(size), transform)
 }
+
+fun Project.findStringProperty(name: String): String? = if (hasProperty(name)) property(name) as String? else null
+
+fun parseRuleNames(ruleNames: String): Set<String> =
+    ruleNames.split(",").map { it.trim() }.filter { it.isNotEmpty()} .toSet()
